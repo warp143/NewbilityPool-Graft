2c5fac53a5c07a62d74a2f8b48f9bea01911731b
Warp143 <AndrewMichaelKho@Andrews-MacBook-6.local>
2018-02-26 05:22:03 +0800

    a

---
+++ Chinese Website/config.js
@@ -0,0 +1,15 @@
+var api = "http://207.148.67.239:8183";
+
+var poolHost = "cn-graft-pool.newbilitypool.com";
+
+var email = "support@newbilitypool.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://blockexplorer.graft.network/block/{id}";
+
+var transactionExplorer = "http://blockexplorer.graft.network/tx/{id}";
+
+var themeCss = "themes/motherboard-dark-theme.css";
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,18 +1,19 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y \
-    build-essential \
-    cmake \
-    pkg-config \
-    libboost-all-dev \
-    libssl-dev \
-    libunbound-dev \
-    libminiupnpc-dev \
-    libunwind8-dev \
-    liblzma-dev \
-    libldns-dev \
-    libexpat1-dev \
-    doxygen \
-    graphviz
+    git -y \
+    build-essential -y \
+    cmake -y \
+    pkg-config -y \
+    libboost-all-dev -y \
+    libssl-dev -y \
+    libunbound-dev -y \
+    libminiupnpc-dev -y \
+    libunwind8-dev -y \
+    liblzma-dev -y \
+    libldns-dev -y \
+    libexpat1-dev -y \
+    doxygen -y \
+    graphviz -y
 
 sudo apt-get install libgtest-dev && cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/
